1670f180efe3416d9756079ccaa4b6a1e2ee12ea
Ajay Gellu Yadav <126907776+Ajaygellu@users.noreply.github.com>
2026-05-30 19:56:15 +0530

    Refactor PDF chatbot title and improve comments

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def load_embeddings():
 
 # ---------------- PAGE CONFIG ----------------
 st.set_page_config(page_title="PDF RAG Chatbot", page_icon="📄")
-st.title("📄 RAG-PDF-Question-Answering-System")
+st.title("📄 PDF Insight")
 
 # ---------------- PDF UPLOAD ----------------
 uploaded_file = st.file_uploader("Upload a PDF to get started", type=["pdf"])
